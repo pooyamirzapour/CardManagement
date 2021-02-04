6692f020e30a8b29017a904abbb88716cf647631
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-02-04 17:58:47 +0330

    init

--- src/main/java/com/digi/Card/CardApplication.java
+++ src/main/java/com/digi/Card/CardApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class CardApplication {
 
+
 	public static void main(String[] args) {
 		SpringApplication.run(CardApplication.class, args);
 	}
