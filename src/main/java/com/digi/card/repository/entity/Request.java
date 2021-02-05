b36f019e92f09b94f739887a0bd8b6e77441ec9c
P.Mirzapour <pooyamirzapour@yahoo.com>
2021-02-05 15:58:33 +0330

    request Response

--- src/main/java/com/digi/card/repository/entity/Request.java
+++ src/main/java/com/digi/card/repository/entity/Request.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "Request")
+@Table(name = "TransferRequest")
 public class Request     {
     private long id;
     private String source;
--- src/main/java/com/digi/card/repository/entity/Response.java
+++ src/main/java/com/digi/card/repository/entity/Response.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.Date;
 
 @Entity
-@Table(name = "Response")
+@Table(name = "TransferResponse")
 public class Response {
     private long id;
     private String status;
